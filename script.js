888df9fde25598def2146e05751ffab3796dd3a1
supragya27 <supragya.chauhan001@gmail.com>
2020-04-12 12:41:16 +0530

    let and const

---
+++ script.js
@@ -0,0 +1,43 @@
+"use strict"
+/*
+
+//  let and const
+var a=10;
+let b=20;
+
+console.log(a);
+console.log(b);  //both work
+
+*/
+/*
+function testVar()
+{
+    var a=50;
+    if(true){
+     var a=20;
+     console.log(a)  //20
+    }
+
+     console.log(a)  //20
+}
+*/
+/*
+function testLet()
+{
+    let a=50;
+    if(true){
+      let a=20;
+      console.log(a); //20
+    }
+
+      console.log(a);  //50
+}
+testLet()
+*/
+/*
+const a=[];
+a.push(10);
+a.push(15);
+// a=10  error
+console.log(a)
+*/
