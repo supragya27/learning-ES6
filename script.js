eba18e1c3064cd0243b8f65e4959ac6652cd2745
supragya27 <supragya.chauhan001@gmail.com>
2020-04-12 12:42:27 +0530

    class and inheritance

--- script.js
+++ script.js
@@ -41,3 +41,48 @@ a.push(15);
 // a=10  error
 console.log(a)
 */
+
+//Class and inheritance
+/*
+class User{
+  static a=50;
+  constructor(name,age,gender)
+  {
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+  }
+  Register()
+  {
+      console.log(this.name +" is registered successfully")
+  }
+  static count()
+  {
+    console.log("Total users registered: "+this.a)
+    //console.log("Total users registered: "+a) gives error
+  }
+}
+
+let o1=new User("Supragya",21,"male")
+o1.Register();
+User.count();
+//o1.count();   gives error
+
+class Member extends User{
+  constructor(name,age,gender,city)
+  {
+      super(name,age,gender);
+      this.city=city;
+  }
+  getCity()
+  {
+    console.log(this.name+"'s city: "+this.city)
+  }
+}
+
+let o2=new Member("Swaranjal",17,"female","Shimla")
+o2.Register();
+Member.count();
+o2.getCity();
+
+*/
