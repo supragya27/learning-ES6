c8060d40064e1f74542ead5b530a73210632aaed
supragya27 <supragya.chauhan001@gmail.com>
2020-04-14 23:00:54 +0530

    promises

--- script.js
+++ script.js
@@ -218,3 +218,16 @@ const sum=(a,b)=>{
 sum(5,4)
 
 */
+
+//Promises
+/* 
+let p=new Promise((resolve,reject)=>{
+  let a=1+1;
+  if(a==2)
+  resolve("Success")
+  else
+  reject("Failed")
+})
+
+p.then((message)=>console.log(message))
+.catch((message)=>console.log(message)) */
